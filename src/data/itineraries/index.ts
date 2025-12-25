bbbae625a1c5447522aff4fdf756b04103cb461b
Jose Tabares <josetabares24@gmail.com>
2025-12-25 22:23:00 +0000

    Limpiar archivos viejos

---
+++ src/data/itineraries/index.ts
@@ -0,0 +1,24 @@
+export const itinerarios = [
+  {
+    slug: "lisboa-1-dia-lo-esencial",
+    title: "Lisboa 1 Dia",
+    description: "Lo esencial de Lisboa",
+    price: 27,
+    duration: 1
+  },
+  {
+    slug: "lisboa-2-dias-completo",
+    title: "Lisboa 2 Dias",
+    description: "Experiencia completa",
+    price: 37,
+    duration: 2,
+    popular: true
+  },
+  {
+    slug: "lisboa-3-dias-premium",
+    title: "Lisboa 3 Dias",
+    description: "Lisboa y alrededores",
+    price: 47,
+    duration: 3
+  }
+];
